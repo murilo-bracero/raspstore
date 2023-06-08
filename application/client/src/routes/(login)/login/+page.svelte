79fbd72507df0c7a4328185c77ea96a8c0211b2d
murilobracero <murilobracero@gmail.com>
2023-06-08 00:26:14 -0300

    initial backend communication logic login

--- application/client/src/routes/(login)/login/+page.svelte
+++ application/client/src/routes/(login)/login/+page.svelte
@@ -1,9 +1,51 @@
+<script lang="ts">
+  import { loginForm, type LoginForm, type LoginResponse } from '../../../stores/login';
+
+  function handleLoginSubmit(event: any) {
+    event.preventDefault();
+    loginForm.subscribe(loginFormSubscriptionAction);
+  }
+
+  function loginFormSubscriptionAction(form: LoginForm) {
+    if (!validateLoginForm(form)) {
+      throw new Error('Login Form invalid');
+    }
+
+    fetch(import.meta.env.VITE_LOGIN_URL, {
+      method: 'POST',
+      headers: { Authorization: 'Basic ' + btoa(`${form.username}:${form.password}`) }
+    })
+      .then((res) => res.json())
+      .then((res: LoginResponse) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function validateLoginForm(form: LoginForm): boolean {
+    return [form.password, form.username].filter((field) => field && field === '').length === 0;
+  }
+</script>
+
 <main class="flex h-screen items-center justify-center">
-  <form class="flex flex-col items-start justify-center rounded-lg border-2 border-black p-6">
+  <form
+    class="flex flex-col items-start justify-center rounded-lg border-2 border-black p-6"
+    on:submit={handleLoginSubmit}
+  >
     <label class="mb-1 p-1 font-bold" for="username">Username:</label>
-    <input class="rounded-lg border-2 border-black p-1" type="text" id="username" />
+    <input
+      class="rounded-lg border-2 border-black p-1"
+      type="text"
+      id="username"
+      bind:value={$loginForm.username}
+    />
     <label class="mb-1 mt-3 p-1 font-bold" for="password">Password:</label>
-    <input class="rounded-lg border-2 border-black p-1" type="password" id="password" />
+    <input
+      class="rounded-lg border-2 border-black p-1"
+      type="password"
+      id="password"
+      bind:value={$loginForm.password}
+    />
     <button
       class="my-3 self-end rounded-full border-2 border-black bg-sky-400 px-3 py-1 font-bold"
       type="submit">Log in</button
