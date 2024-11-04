f64c681d90768cb9f97bfd7760c71e92de6866a5
murilobracero <murilobracero@gmail.com>
2024-11-04 14:25:00 -0300

    fixed folder conficts

--- raspstore-api/internal/auth/token_test.go
+++ raspstore-api/internal/auth/token_test.go
@@ -1,10 +1,10 @@
-package auth
+package auth_test
 
 import (
 	"context"
-	"os"
 	"testing"
 
+	"github.com/murilo-bracero/raspstore/file-service/internal/auth"
 	"github.com/murilo-bracero/raspstore/file-service/internal/infra/bootstrap"
 	"github.com/murilo-bracero/raspstore/file-service/internal/infra/config"
 	"github.com/stretchr/testify/assert"
@@ -12,21 +12,21 @@ import (
 
 func TestGenerateRefreshToken(t *testing.T) {
 
-	mockConfig := &config.Config{Storage: config.StorageConfig{Path: os.TempDir()}}
+	mockConfig := &config.Config{Storage: config.StorageConfig{Path: t.TempDir()}}
 
-	(&bootstrap.FolderBootstraper{}).Bootstrap(context.Background(), mockConfig)
+	err := (&bootstrap.FolderBootstraper{}).Bootstrap(context.Background(), mockConfig)
 
-	(&bootstrap.SecretsBootstraper{}).Bootstrap(context.Background(), mockConfig)
+	assert.NoError(t, err, "FolderBootstraper")
+
+	err = (&bootstrap.SecretsBootstraper{}).Bootstrap(context.Background(), mockConfig)
+
+	assert.NoError(t, err, "SecretsBootstraper")
 
 	t.Run("should generate refresh token", func(t *testing.T) {
-		token, err := GenerateRefreshToken(mockConfig)
+		token, err := auth.GenerateRefreshToken(mockConfig)
 
 		assert.NoError(t, err, "GenerateRefreshToken")
 
 		assert.NotEmpty(t, token, "GenerateRefreshToken")
-	})
-
-	t.Cleanup(func() {
-		os.RemoveAll(os.TempDir() + "/secrets")
 	})
 }
